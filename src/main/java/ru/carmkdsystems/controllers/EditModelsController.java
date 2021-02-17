6b9f4fd29787efcba2c8545aa844d07b0c52c44f
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-02-18 02:01:41 +0300

    1

--- src/main/java/ru/carmkdsystems/controllers/EditModelsController.java
+++ src/main/java/ru/carmkdsystems/controllers/EditModelsController.java
@@ -23,50 +23,38 @@ public class EditModelsController {
     }
 
     @PostMapping
-    public String addFirmCar(@RequestParam("firm") String firm,
-                             Model model) {
-        if (!firm.isEmpty() && firm.length() > 64) {
-            model.addAttribute("errorText", "Слишком длинное название фирмы производителя");
-            model.addAttribute("allFirms", firmCarRepos.findAll());
-            return "editModels";
-        }
-        if (!firm.isEmpty()) {
-            if (firmCarRepos.findByFirm(firm) != null) {
-                model.addAttribute("errorText", "Эта фирма уже существует!");
+    public String postEditList(@RequestParam(value = "firm", required = false) String firm,
+                               @RequestParam(value = "id", required = false) Long id,
+                               @RequestParam("task") int task,
+                               Model model) {
+        if (task == 1 || task == 2) {
+            if (!firm.isEmpty() && firm.length() > 64) {
+                model.addAttribute("errorText", "Слишком длинное название фирмы производителя");
                 model.addAttribute("allFirms", firmCarRepos.findAll());
                 return "editModels";
             }
-            FirmCar firmCar = new FirmCar();
-            firmCar.setFirm(firm);
-            firmCarRepos.save(firmCar);
-            model.addAttribute("successText", String.format("Фирма %s успешно создана!", firm));
-            model.addAttribute("allFirms", firmCarRepos.findAll());
-        }
-        return "editModels";
-    }
-
-    @PostMapping
-    public String editFirmCar(@RequestParam("firm") String firm,
-                              @RequestParam("id") Long id,
-                             Model model) {
-        if (!firm.isEmpty() && firm.length() > 64) {
-            model.addAttribute("errorText", "Слишком длинное название фирмы производителя");
-            model.addAttribute("allFirms", firmCarRepos.findAll());
-            return "editModels";
-        }
-        if (!firm.isEmpty()) {
-            if (firmCarRepos.findByFirm(firm) != null) {
-                model.addAttribute("errorText", "Эта фирма уже существует!");
-                model.addAttribute("allFirms", firmCarRepos.findAll());
-                return "editModels";
+            if (!firm.isEmpty()) {
+                if (firmCarRepos.findByFirm(firm) != null) {
+                    model.addAttribute("errorText", "Эта фирма уже существует!");
+                    model.addAttribute("allFirms", firmCarRepos.findAll());
+                    return "editModels";
+                }
+                FirmCar firmCar;
+                if (task == 1) {
+                    firmCar = new FirmCar();
+                    firmCar.setFirm(firm);
+                    firmCarRepos.save(firmCar);
+                    model.addAttribute("successText", String.format("Фирма %s успешно создана!", firm));
+                } else {
+                    firmCar = firmCarRepos.findById(id).get();
+                    String oldFirm = firmCar.getFirm();
+                    firmCar.setFirm(firm);
+                    firmCarRepos.save(firmCar);
+                    model.addAttribute("successText", String.format("Фирма %s успешно изменена на %s!", oldFirm, firm));
+                }
             }
-            FirmCar firmCar = firmCarRepos.findById(id).get();
-            String oldFirm = firmCar.getFirm();
-            firmCar.setFirm(firm);
-            firmCarRepos.save(firmCar);
-            model.addAttribute("successText", String.format("Фирма %s успешно изменена на %s!", oldFirm, firm));
-            model.addAttribute("allFirms", firmCarRepos.findAll());
         }
+        model.addAttribute("allFirms", firmCarRepos.findAll());
         return "editModels";
     }
 }
