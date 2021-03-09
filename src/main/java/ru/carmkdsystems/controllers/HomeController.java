ad446439ce1a1b40af22ec30efd4fd3e1760e6b7
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-03-09 10:38:01 +0300

    1

--- src/main/java/ru/carmkdsystems/controllers/HomeController.java
+++ src/main/java/ru/carmkdsystems/controllers/HomeController.java
@@ -1,10 +1,15 @@
 package ru.carmkdsystems.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.carmkdsystems.repositories.ProductRepos;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -13,8 +18,14 @@ public class HomeController {
 	private ProductRepos productRepos;
 
 	@GetMapping("/")
-	public String home(Model model) {
-		model.addAttribute("AllProducts", productRepos.findAll());
+	public String home(Model model,
+					   HttpServletRequest user,
+					   @PageableDefault(sort = { "id" }, direction = Sort.Direction.DESC) Pageable pageable) {
+		if (user != null && user.isUserInRole("ADMIN")) {
+			model.addAttribute("AllProducts", productRepos.findAllDesc(pageable));
+		} else {
+			model.addAttribute("AllProducts", productRepos.findAllByIsProduct(pageable));
+		}
 		return "home";
 	}
 }
--- src/main/java/ru/carmkdsystems/repositories/ProductRepos.java
+++ src/main/java/ru/carmkdsystems/repositories/ProductRepos.java
@@ -1,8 +1,16 @@
 package ru.carmkdsystems.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.carmkdsystems.models.Product;
 
 public interface ProductRepos extends CrudRepository<Product, Long> {
 
+    @Query(value = "SELECT * FROM product ORDER BY id DESC", nativeQuery = true)
+    Page<Product> findAllDesc(Pageable pageable);
+
+    @Query(value = "SELECT * FROM product WHERE is_public = true ORDER BY id DESC", nativeQuery = true)
+    Page<Product> findAllByIsProduct(Pageable pageable);
 }
