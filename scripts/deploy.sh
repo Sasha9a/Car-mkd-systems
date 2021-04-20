dfcbe7b6dd48b88154f3386e4a17d36d4f23edb3
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-04-20 16:20:16 +0300

    finish

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ scp -i ~/.ssh/id_rsa \
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/id_rsa root@45.90.35.174 << EOF
+ssh -tt -i ~/.ssh/id_rsa root@45.90.35.174 << EOF
   pkill -f car-mkd-systems-0.0.1.jar
   nohup java -jar car-mkd-systems-0.0.1.jar > log.txt 2>&1 &
   exit
