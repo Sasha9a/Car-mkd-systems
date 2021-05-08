7345221f376bcae4532488ea44f84f9151f35cf7
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-05-08 23:30:12 +0300

    Пофиксил характеристики

--- src/main/java/ru/carmkdsystems/controllers/EditParamsController.java
+++ src/main/java/ru/carmkdsystems/controllers/EditParamsController.java
@@ -7,53 +7,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.carmkdsystems.models.ProductParams;
-import ru.carmkdsystems.repositories.ProductParamsRepos;
+import ru.carmkdsystems.models.Params;
+import ru.carmkdsystems.repositories.ParamsRepos;
 
 @Controller
 @RequestMapping("/edit-params")
 public class EditParamsController {
 
     @Autowired
-    private ProductParamsRepos productParamsRepos;
+    private ParamsRepos paramsRepos;
 
     @GetMapping
     public String showHtml(Model model) {
-        model.addAttribute("allProductParams", productParamsRepos.findAll());
+        model.addAttribute("allParams", paramsRepos.findAll());
         return "editParams";
     }
 
     @PostMapping
     public String editParams(@RequestParam(value = "name", required = false) String name,
-                             @RequestParam(value = "type", required = false) String type,
                              @RequestParam(value = "id", required = false) Long id,
                              Model model) {
-        if (name != null && type != null) {
-            if (!name.isEmpty() && !type.equals("Выберите тип...")) {
+        if (name != null) {
+            if (!name.isEmpty()) {
                 if (name.length() > 100) {
                     model.addAttribute("errorText", "Слишком длинное название характеристики!");
-                    model.addAttribute("allProductParams", productParamsRepos.findAll());
+                    model.addAttribute("allProductParams", paramsRepos.findAll());
                     return "editParams";
                 }
-                if (productParamsRepos.findByName(name) != null) {
+                if (paramsRepos.findByName(name) != null) {
                     model.addAttribute("errorText", "Такая характеристика уже существует!");
-                    model.addAttribute("allProductParams", productParamsRepos.findAll());
+                    model.addAttribute("allProductParams", paramsRepos.findAll());
                     return "editParams";
                 }
-                ProductParams productParams = new ProductParams(name, type);
-                productParamsRepos.save(productParams);
+                Params params = new Params(name);
+                paramsRepos.save(params);
                 model.addAttribute("successText", String.format("Характеристика <b>%s</b> успешно создана!", name));
             } else {
                 model.addAttribute("errorText", "Вы не ввели все данные при создании!");
             }
         } else if (id != null) {
-            if (productParamsRepos.findById(id).isPresent()) {
-                String namePar = productParamsRepos.findById(id).get().getName();
-                productParamsRepos.deleteById(id);
+            if (paramsRepos.findById(id).isPresent()) {
+                String namePar = paramsRepos.findById(id).get().getName();
+                paramsRepos.deleteById(id);
                 model.addAttribute("successText", String.format("Характеристика <b>%s</b> успешно удалена!", namePar));
             }
         }
-        model.addAttribute("allProductParams", productParamsRepos.findAll());
+        model.addAttribute("allParams", paramsRepos.findAll());
         return "editParams";
     }
 }
