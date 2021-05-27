4d3099a1a05013f2a1ee55d613613b47452a7b0e
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-05-27 12:11:33 +0300

    Полностью готов сайт

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,10 +3,10 @@ echo 'Connect to Server...'
 ssh -tt -i ~/.ssh/id_rsa root@45.141.78.161 << EOF
 
 pm2 stop 0
-cp Car-mkd-systems/public/images images
+cp -r Car-mkd-systems/public/images images
 rm -rf Car-mkd-systems
 git clone https://github.com/Sasha9a/Car-mkd-systems.git -b v1.0
-cp images Car-mkd-systems/public/images
+cp -r images Car-mkd-systems/public/images
 rm -rf images
 cd Car-mkd-systems
 npm install
