a403f656418c2688852591172f9cc0b92f79ddee
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-06-14 19:50:42 +0300

    v1.1

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,7 +5,7 @@ ssh -tt -i ~/.ssh/id_rsa root@45.141.78.161 << EOF
 pm2 stop 0
 cp -r Car-mkd-systems/public/images images
 rm -rf Car-mkd-systems
-git clone https://github.com/Sasha9a/Car-mkd-systems.git -b v1.0
+git clone https://github.com/Sasha9a/Car-mkd-systems.git -b v1.1
 cp -r images Car-mkd-systems/public/images
 rm -rf images
 cd Car-mkd-systems
