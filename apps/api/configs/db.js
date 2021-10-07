474921e84e4901864cb1ecff46049761b7f400c4
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-10-07 23:48:37 +0300

    Настройка проекта

---
+++ apps/api/configs/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  db: 'mongodb://localhost:27017/car',
+  secret: '8bd93419352231fbc30123424b8709e1'
+}
--- apps/api/index.js
+++ apps/api/index.js
@@ -1,1 +1,33 @@
+// Подключение библиотек
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 
+// Подключение конфигов
+const db = require('./configs/db');
+
+// Подключение роутов
+const accountRoute = require('./routes/account.route');
+
+const app = express();
+const port = 3000;
+
+// Настройка проекта
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Привязка роутов
+app.use('/account', accountRoute);
+
+mongoose.connect(db.db, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) {
+    console.error(`Не удалось подключиться к БД: ${err}`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Сервер был запущен по порту ${port}`);
+});
