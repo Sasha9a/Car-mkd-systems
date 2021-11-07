d4207572711b0c7e076d958160b59d6859dfd640
jlyessa <jlyessa@student.21-school.ru>
2021-11-07 13:05:41 +0300

    Настройка GraphQL

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { environment } from "../environments/environment";
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppResolver } from "./app.resolver";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...environment.connection
+    }),
+    GraphQLModule.forRoot({
+      typePaths: ["./**/*.graphql"],
+      context: ({ req }) => ({ req }),
+      playground: true
     })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppResolver],
 })
 export class AppModule {
 }
