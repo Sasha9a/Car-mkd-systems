166f1f5defce62e6aef31f8df8224dfd2fd5ab34
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-11-13 22:49:49 +0300

    Настройка логина

--- apps/api/src/app/modules/user/user.module.ts
+++ apps/api/src/app/modules/user/user.module.ts
@@ -3,10 +3,30 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User, UserSchema } from '@car-mkd-systems/shared/schemas/user.schema';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from '@car-mkd-systems/modules/user/jwt.strategy';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: true
+    }),
+    JwtModule.register({
+      secret: 'd2103dfe7288ccb50a4a7af9ff90ec52',
+      signOptions: {
+        expiresIn: 3600 * 24 * 30
+      }
+    })
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, JwtStrategy],
+  exports: [
+    PassportModule,
+    JwtModule
+  ]
 })
 export class UserModule {}
