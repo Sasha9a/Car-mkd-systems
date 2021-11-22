5c47fd51a262921ac0338d89aa4db8e0e6286211
adovgiy <adovgiy@zaoblako.ru>
2021-11-22 18:46:27 +0300

    login + Настройка TS

---
+++ apps/web/src/app/modules/login/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'car-login',
+  templateUrl: './login.component.html',
+  styleUrls: []
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
