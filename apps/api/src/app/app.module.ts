0fcc9b1385e327995b8e9524ed7530d23bed9d7e
adovgiy <adovgiy@zaoblako.ru>
2021-12-03 18:12:52 +0300

    Панель категорий.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { LoggerMiddleware } from '@car-mkd-systems/api/core/middlewares/logger.middleware';
+import { CategoryModule } from '@car-mkd-systems/api/modules/category/category.module';
 import { ModelCarModule } from '@car-mkd-systems/api/modules/model-car/model.car.module';
 import { UserModule } from '@car-mkd-systems/api/modules/user/user.module';
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
@@ -10,7 +11,8 @@ import { environment } from '../environments/environment';
   imports: [
     MongooseModule.forRoot(environment.connection.db),
     UserModule,
-    ModelCarModule
+    ModelCarModule,
+    CategoryModule
   ],
   controllers: [],
   providers: []
