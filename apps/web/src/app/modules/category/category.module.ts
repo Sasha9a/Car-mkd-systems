6fc5743818b9d5d032086efcacc918a1e877b9af
adovgiy <adovgiy@zaoblako.ru>
2021-12-03 18:03:23 +0300

    Панель категорий.

--- apps/web/src/app/modules/category/category.module.ts
+++ apps/web/src/app/modules/category/category.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { ShareModule } from "@car-mkd-systems/web/shared/share.module";
+import { AccordionModule } from 'primeng/accordion';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+import { RippleModule } from 'primeng/ripple';
 import { CategoryComponent } from './components/category/category.component';
 
 const categoryRoutes: Routes = [
@@ -16,7 +24,15 @@ const categoryRoutes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(categoryRoutes)
+    RouterModule.forChild(categoryRoutes),
+    ShareModule,
+    PanelModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule,
+    CheckboxModule,
+    AccordionModule,
+    RippleModule
   ]
 })
 export class CategoryModule { }
