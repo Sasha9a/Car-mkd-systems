5cbde1184a292d0783f47662972d3c4fa141ac35
adovgiy <adovgiy@zaoblako.ru>
2021-12-06 10:17:50 +0300

    Создал роли

--- libs/shared/src/schemas/user.schema.ts
+++ libs/shared/src/schemas/user.schema.ts
@@ -1,3 +1,4 @@
+import { RoleEnum } from '@car-mkd-systems/shared/enums/role.enum';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
@@ -8,6 +9,9 @@ export class User extends Document {
 
   @Prop({ required: true })
   public password: string;
+
+  @Prop({ required: true })
+  public roles: RoleEnum[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
