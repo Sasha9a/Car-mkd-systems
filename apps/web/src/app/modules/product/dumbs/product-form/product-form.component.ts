7dfde1ced2055c4a62b93955b058d2dd82bef95f
adovgiy <adovgiy@zaoblako.ru>
2021-12-10 18:37:14 +0300

    Создание товара

--- apps/web/src/app/modules/product/dumbs/product-form/product-form.component.ts
+++ apps/web/src/app/modules/product/dumbs/product-form/product-form.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductFormDto } from '@car-mkd-systems/shared/dtos/product/product.form.dto';
+import { BaseFormComponent } from '@car-mkd-systems/web/shared/dumbs/base-form/base-form.component';
 
 @Component({
   selector: 'car-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent extends BaseFormComponent<ProductFormDto> {
 
-  constructor() { }
+  @Input() public product: ProductFormDto = new ProductFormDto();
+  public dto = ProductFormDto;
 
-  ngOnInit(): void {
+  public constructor() {
+    super();
   }
 
 }
