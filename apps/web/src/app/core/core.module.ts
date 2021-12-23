10e3a63b32e9cda700a162a4da709f947a3a355d
adovgiy <adovgiy@zaoblako.ru>
2021-12-23 13:13:23 +0300

    Помощь

--- apps/web/src/app/core/core.module.ts
+++ apps/web/src/app/core/core.module.ts
@@ -45,6 +45,15 @@ const routes: Routes = [
       {
         path: 'product',
         loadChildren: () => import('../modules/product/product.module').then(m => m.ProductModule)
+      },
+      {
+        path: 'help',
+        canActivate: [AuthGuard, RoleGuard],
+        loadChildren: () => import('../modules/help/help.module').then(m => m.HelpModule),
+        data: {
+          roles: [RoleEnum.ADMIN],
+          included: true
+        }
       }
     ]
   }
