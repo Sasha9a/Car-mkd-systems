bfaefd31dbd85be229dd461795e25ba9d7c5348f
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-12-30 21:52:41 +0300

    deploy

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,7 +7,7 @@ cd Car-mkd-systems
 git pull
 npm install
 nx affected:build --all
-sudo cp /etc/nginx/sites-available/car-mkd-systems.ru deploy/nginx.conf
+sudo cp deploy/nginx.conf /etc/nginx/sites-available/car-mkd-systems.ru
 sudo ln -s /etc/nginx/sites-available/car-mkd-systems.ru /etc/nginx/sites-enabled/
 sudo cp -r ~/Car-mkd-systems/dist/apps/web/* /var/www/car-mkd-systems.ru/html
 sudo systemctl restart nginx
