b4aea01f01dece7dad90718731175eabe4302454
Sasha9a <aleksandr_dovgiyy@mail.ru>
2021-12-31 16:06:01 +0300

    deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,28 +4,23 @@
  */
 
 import { NestFactory } from '@nestjs/core';
-import {ExpressAdapter} from "@nestjs/platform-express";
-import * as http from "http";
-import * as https from "https";
 
 import { AppModule } from './app/app.module';
-import * as fs from 'fs';
-import * as express from 'express';
 
 async function bootstrap() {
-  const server = express();
-  const privateKey = fs.readFileSync(__dirname + '/../../../deploy/server.key', 'utf8');
-  const certificate = fs.readFileSync(__dirname + '/../../../deploy/server.crt', 'utf8');
-  const httpsOptions = { key: privateKey, cert: certificate };
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: __dirname + '/../../../deploy/server.key',
+      cert: __dirname + '/../../../deploy/server.crt'
+    }
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
   const port = process.env['PORT'] || 3000;
-  await app.init();
-
-  http.createServer(server).listen(port);
-  https.createServer(httpsOptions, server).listen(443);
+  await app.listen(port, () => {
+    console.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  });
 }
 
 bootstrap().catch(console.error);
