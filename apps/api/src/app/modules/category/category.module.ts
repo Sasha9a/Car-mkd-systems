db31388d6acbc05a22fb624107c383d5fa7b1bc6
adovgiy <adovgiy@zaoblako.ru>
2022-03-14 15:33:24 +0300

    api категории

--- apps/api/src/app/modules/category/category.module.ts
+++ apps/api/src/app/modules/category/category.module.ts
@@ -3,13 +3,12 @@ import { CategoryService } from '@car-mkd-systems/api/modules/category/category.
 import { ProductModule } from '@car-mkd-systems/api/modules/product/product.module';
 import { UserModule } from '@car-mkd-systems/api/modules/user/user.module';
 import { Category, CategorySchema } from '@car-mkd-systems/shared/schemas/category.schema';
-import { Characteristic, CharacteristicSchema } from '@car-mkd-systems/shared/schemas/characteristic.schema';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }, { name: Characteristic.name, schema: CharacteristicSchema }]),
+    MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }]),
     UserModule,
     ProductModule
   ],
