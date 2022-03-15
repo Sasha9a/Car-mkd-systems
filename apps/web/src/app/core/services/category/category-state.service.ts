d68220b498ac55f22dd6d6474669cda136175a2e
adovgiy <adovgiy@zaoblako.ru>
2022-03-15 18:13:14 +0300

    Товары

--- apps/web/src/app/core/services/category/category-state.service.ts
+++ apps/web/src/app/core/services/category/category-state.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CategoryDto } from '@car-mkd-systems/shared/dtos/category/category.dto';
 import { BaseStateService } from '@car-mkd-systems/web/core/services/base-state.service';
 import { CategoryService } from '@car-mkd-systems/web/core/services/category/category.service';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +10,6 @@ export class CategoryStateService extends BaseStateService {
   public constructor(private readonly categoryService: CategoryService) {
     super();
     this.baseService = categoryService;
-  }
-
-  public findAllDropdown(): Observable<CategoryDto[]> {
-    return this.categoryService.findAllDropdown();
   }
 
 }
--- apps/web/src/app/core/services/category/category.service.ts
+++ apps/web/src/app/core/services/category/category.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CategoryDto } from '@car-mkd-systems/shared/dtos/category/category.dto';
 import { BaseService } from '@car-mkd-systems/web/core/services/base.service';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService extends BaseService {
-  protected override baseUrl = '/category';
 
-  public findAllDropdown(): Observable<CategoryDto[]> {
-    return this.http.get<CategoryDto[]>(`${this.baseUrl}/all`);
-  }
+  protected override baseUrl = '/category';
 
 }
