ad27b1ea44a08f6c5346b42af99e196d5ba8f05b
Sasha9a <aleksandr_dovgiyy@mail.ru>
2022-03-20 22:38:04 +0300

    Обновление деплоя

--- deploy/start.sh
+++ deploy/start.sh
@@ -15,6 +15,19 @@ echo 'Connect to Server...'
 # SSL сертификат
 # sudo certbot --nginx -d car-mkd-systems.ru -d www.car-mkd-systems.ru; sudo pm2 restart 0
 
+# Если npm install завершается Killed
+# sudo fallocate -l 1G /swapfile
+# sudo chmod 600 /swapfile
+# sudo mkswap /swapfile
+# sudo swapon /swapfile
+# sudo swapon --show
+# sudo cp /etc/fstab /etc/fstab.bak
+# echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+# sudo sysctl vm.swappiness=10
+# echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+# sudo sysctl vm.vfs_cache_pressure=50
+# echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
+
 umask 777
 ssh -tt -i ~/.ssh/id_rsa root@45.141.78.161 << EOF
 sudo npm install -g pm2
