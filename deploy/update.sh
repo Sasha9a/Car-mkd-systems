4c630889cf545f2af5a72446c7620d6131766275
Sasha9a <aleksandr_dovgiyy@mail.ru>
2022-03-28 22:05:34 +0300

    Обновление деплоя

--- deploy/update.sh
+++ deploy/update.sh
@@ -19,7 +19,6 @@ echo 'Connect to Server...'
 
 umask 777
 ssh -tt -i ~/.ssh/id_rsa root@45.141.78.161 << EOF
-sudo pm2 stop 0
 cd Car-mkd-systems
 sudo git pull
 sudo npm install --unsafe
@@ -27,7 +26,6 @@ nx affected:build --all
 sudo cp deploy/nginx.conf /etc/nginx/sites-available/car-mkd-systems.ru
 sudo ln -s /etc/nginx/sites-available/car-mkd-systems.ru /etc/nginx/sites-enabled/
 sudo cp -r ~/Car-mkd-systems/dist/apps/web/* /var/www/car-mkd-systems.ru/html
-sudo certbot --nginx -d car-mkd-systems.ru -d www.car-mkd-systems.ru
 sudo systemctl restart nginx
 sudo pm2 restart 0
 exit
